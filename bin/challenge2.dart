7ef373c0727ebd3bdde89530a557765148243bc7
Muhammad Farabie <farabie024@gmail.com>
2023-08-28 14:09:15 +0700

    Challenge 2 AWB

---
+++ bin/challenge2.dart
@@ -0,0 +1,19 @@
+void main() {
+  //Menghitung keliling persegi
+  int pp = 12;
+  int kelilingPersegi = 4 * pp;
+  print('Keliling Persegi: $kelilingPersegi');
+
+  //Menghitung luas lingkaran
+  int rl = 4;
+  double luasLingkaran = 3.14 * rl * rl;
+  print('Luas Lingkaran: $luasLingkaran');
+
+  //menghitung volume balok
+  int pb = 4;
+  int lb = 7;
+  int tb = 3;
+
+  int volumeBalok = pb * lb * tb;
+  print('Volume Balok: $volumeBalok');
+}
