72b93f4f9d8c5078e6c9a4ce7c15714832540941
Muhammad Farabie <farabie024@gmail.com>
2023-08-28 10:08:12 +0700

    Operator

---
+++ bin/operator.dart
@@ -0,0 +1,62 @@
+void main() {
+  var a = 12;
+  var b = 8;
+  var c = a + b;
+
+  //Operands -> Representasi dari data (a dan b)
+  //Operator -> sesuatu yang memutuskan bagaimana operands akan di proses
+
+  //Aritmathic Operator
+
+  print('Operator Aritmatic');
+
+  //Penjumlahan
+  print(a + b);
+
+  //Pengurangan
+  print(a - b);
+
+  //Perkalian
+  print(a * b);
+
+  //Pembagian
+  print(a / b);
+
+  //Sisa bagi
+  print(a % b);
+
+  //Equality & Relational Operator
+
+  print('Operator Equality & Relational Operator');
+
+  //lebih dari
+  print(a > b);
+
+  //kurang dari
+  print(a < b);
+
+  //Lebih dari sama dengan
+  print(a <= b);
+
+  //kurang dari sama dengan
+  print(a >= b);
+
+  //Sama dengan
+  print(a == b);
+
+  //Tidak sama dengan
+  print(a != b);
+
+  // Logical Operator
+  var x = true;
+  var y = false;
+
+  //&& AND -> Operator yang jika keduanya true maka akan true
+  print(x && y);
+
+  //|| OR -> Operator yang jika salah satu true maka akan bernilai true
+  print(x || y);
+
+  //Operator ! NOT-> Akan bernilai berlawanan
+  print(!x);
+}
